5b4554b16fab5f968cd6d08d6ecb04ac062f6174
Justina <44163331+JustinaJur@users.noreply.github.com>
2019-01-15 16:20:01 +0200

    added responsiveness

--- memoryGame.js
+++ memoryGame.js
@@ -5,8 +5,8 @@ bridges : ["mindaugo", "pontoninis", "zaliasis", "antokol", "uzupio", "zveryno",
 "pontoninis", "zaliasis", "antokol", "uzupio", "zveryno"],
 restaurants : ["dainava", "draugyste", "literatu", "medininkai", "neringa", "nykstukas", 
 "dainava", "draugyste", "literatu", "medininkai", "neringa", "nykstukas"],
-transport : ["arklinis", "autobusas", "saurer", "katedra", "roges", "vezikas", 
-"arklinis", "autobusas", "saurer", "katedra", "roges", "vezikas"]
+transport : ["arklinis", "autobusas", "saurer", "katedra", "roges", "gelezinkelis", 
+"arklinis", "autobusas", "saurer", "katedra", "roges", "gelezinkelis"]
 }
 
 // transport : ["arklinis", "autobusas", "arklinis", "autobusas" ]
@@ -14,14 +14,13 @@ transport : ["arklinis", "autobusas", "saurer", "katedra", "roges", "vezikas",
 
 
 
-
-var matchedCards = [];
-var clickedCards = [];
 var pics = document.querySelectorAll(".pics");
 var buttons = document.querySelectorAll(".topic");
 var container = document.querySelector("#container");
 var redFlag = document.querySelector(".redFlag");
 var announcement = document.querySelector(".announcement");
+var matchedCards = [];
+var clickedCards = [];
 var topic = [];
 var playing = false;
 var allowedGallery = false;
@@ -33,8 +32,6 @@ for (var i = 0; i<buttons.length; i++){
 			    allowedGallery = false;
 			    //remove red color
 				redFlag.classList.remove("attention");
-				
-				
 				// get selected topic from topics object
 		        topic = topics[this.textContent];
 				reset();
@@ -45,11 +42,14 @@ for (var i = 0; i<buttons.length; i++){
 	   
 	   function play(){ 
 	   for (var i = 0; i < pics.length; i++){
-		  pics[i].addEventListener("click", function(){    
+		  pics[i].addEventListener("click", function(){  
+            // open clicked card if number of clicked cards is 0 and prevent opening of matched card 
 		   if(clickedCards.length == 0 && matchedCards.indexOf(this.id) == - 1){
 			  this.setAttribute("src", "images/" + topic[this.id] + ".jpg");
+               // push to clickedCards arr
 			   clickedCards.push(this);
 			  }
+            // open clicked card if number of clicked cards is 1, prevent opening of matched card and previously clicked
 		   if(clickedCards.length == 1 && matchedCards.indexOf(this.id) == - 1 && this.id !== clickedCards[0].id){
 			   this.setAttribute("src", "images/" + topic[this.id] + ".jpg");
 			   clickedCards.push(this);
@@ -60,24 +60,23 @@ for (var i = 0; i<buttons.length; i++){
  
    // compare and count clicked cards
   function compareCards(first, second){
+        
 	     if(first.src === second.src){
+              //if two clicked cards are not in matchedCards array, push them there 
 			 if(matchedCards.every(item => item !== first.id && item !== second.id)){
-				 matchedCards.push(first.id); console.log(first.id);
-				 matchedCards.push(second.id); console.log(second.id);
+				 matchedCards.push(first.id); 
+				 matchedCards.push(second.id);
 		    }
 			setTimeout(function(){
 				first.setAttribute("src", "images/" + "black.jpg")
 				second.setAttribute("src", "images/" + "black.jpg");}, 500);
-				
+			// if all cards are matched, open them all 
 			if(matchedCards.length == topic.length){
-				// allowedGallery = true;
-				console.log(matchedCards);
-				/////////////////////////////////
-				 openAllCards(topic);
-				
+				 openAllCards(topic);			
 	}
 				smallReset();
 		   }else{
+                 //if 2 opened cards are not equal, close them
 				setTimeout(function(){
 					first.setAttribute("src", "images/" + "white.jpg");
 					second.setAttribute("src", "images/" + "white.jpg")}, 500);	  
@@ -89,14 +88,17 @@ for (var i = 0; i<buttons.length; i++){
     
 	 function openAllCards(topic){
 		 setTimeout(function (){
-			 for(var i = 0; i<pics.length; i++){		 
-		 pics[i].setAttribute("src", "images/" + topic[i] + ".jpg")};
+			 for(var i = 0; i<pics.length; i++){	
+          //open large pictures	 
+		 pics[i].setAttribute("src", "lg/" + topic[i] + ".jpg")};
 		 announcement.classList.remove("hidden");
 		 startGallery();
 		 }, 700); 
 	 }
+	 
 	 //////////////////////////////////////////////////////////////////////////////////
 	//// ********* start gallery after all cards are matched 
+
 const outerOverlay = document.querySelector(".outer-overlay");	
 const overlay = document.querySelector(".overlay");
 let overlayPic = document.querySelector(".overlay").getElementsByTagName("img")[0];
@@ -119,14 +121,15 @@ function startGallery(){
 		let src = e.target.src;
 		 //show overlay
 		 
-		if (allowedGallery) {outerOverlay.classList.add("open");
-		                     overlay.classList.add("open");}
+		if (allowedGallery) {
+			outerOverlay.classList.add("open");
+		    overlay.classList.add("open")
+			;}
 			// show clicked picture on overlay
 			overlayPic.src = src;
 			//hide gallery announcement 
 			announcement.classList.add("hidden");
-			     
-				
+			    
 				 //show next pic
 				nextButton.onclick = function() {
 				 // add 1 to id of current pic	
@@ -144,12 +147,9 @@ function startGallery(){
 			   overlayPic.src = pics[counter].src;
 			   
 		}
-		}
+}
 
-		
-		
-		
-		  //close exposition
+		  //close gallery
 	      closeButton.addEventListener("click", close);
 		  function close() {
           overlay.classList.remove("open");
@@ -160,10 +160,7 @@ function startGallery(){
 	 ///////////////////////////////////////////////////////////////////////////////////
 
 	 
-	 
-	 
-	 
-	 
+	  
 	 
 function reset(){
 	pics = Array.from(pics);// return arr
